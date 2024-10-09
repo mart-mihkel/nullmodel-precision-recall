ddc8af10abcd7c3db534fab139e4ebfe50c50608
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-10-09 18:22:27 +0300

    variable treshold for rigged models

--- nullmodel_precision_recall/simulate.py
+++ nullmodel_precision_recall/simulate.py
@@ -4,14 +4,14 @@ from sklearn.metrics import precision_recall_curve
 from plotnine import ggplot, aes, labs, geom_step
 
 
-def __randomize_score(s, y=None, acc=0.0):
+def __randomize_score(s, y=None, acc=0.0, tresh=0.5):
     """
     Randomly permute scores. Rig the scores for the model to have accuracy of
-    at least `acc` with treshold of 0.5 if provided.
+    at least `acc` with treshold of `tresh` if provided.
     """
     s = np.random.permutation(s)
 
-    if acc == 0 or y is None:
+    if y is None or acc == 0:
         return s
 
     n_rig = int(y.shape[0] * acc)
@@ -21,10 +21,23 @@ def __randomize_score(s, y=None, acc=0.0):
     yneg_i = np.random.choice(np.flatnonzero(y == 0), n_neg)
     ypos_i = np.random.choice(np.flatnonzero(y == 1), n_pos)
 
-    sneg_i = np.random.choice(np.flatnonzero(s < 0.5), n_neg)
-    spos_i = np.random.choice(np.flatnonzero(s >= 0.5), n_pos)
+    sneg_i = np.flatnonzero(s < tresh)
+    spos_i = np.flatnonzero(s >= tresh)
 
-    s[yneg_i], s[ypos_i] = s[sneg_i], s[spos_i]
+    neg = s[sneg_i]
+    pos = s[spos_i]
+
+    if neg.shape[0] < n_neg:
+        neg = np.append(neg, np.repeat(1 - tresh, n_neg - neg.shape[0]))
+    else:
+        neg = np.random.choice(neg, n_neg)
+
+    if pos.shape[0] < n_pos:
+        pos = np.append(pos, np.repeat(tresh, n_pos - pos.shape[0]))
+    else:
+        pos = np.random.choice(pos, n_pos)
+
+    s[yneg_i], s[ypos_i] = neg, pos
 
     return s
 
@@ -40,7 +53,8 @@ def __decreasing(p, r):
 def simulate_nullmodels(n_sim: int,
                         n_samp: int,
                         ppos=0.5,
-                        acc=0.0) -> tuple[np.ndarray, np.ndarray]:
+                        acc=0.0,
+                        tresh=0.5) -> tuple[np.ndarray, np.ndarray]:
     """
     Simulate nullmodels
 
@@ -56,9 +70,14 @@ def simulate_nullmodels(n_sim: int,
         Default is 0.5.
     acc : float
         Percentage of samples the nullmodel is rigged to label correctly with
-        a treshold of 0.5.
+        a treshold of `tresh`.
         Must be between 0 and 1.
         Defualt is 0.0.
+    tresh : float
+        Classification treshold when using `acc` to rig the model. Consider
+        a data point positive when `y_score >= tresh`.
+        Must be between 0 and 1.
+        Defualt is 0.5.
 
     Returns
     -------
@@ -72,7 +91,7 @@ def simulate_nullmodels(n_sim: int,
     init_scores = np.tile(np.linspace(0, 1, num=n_samp), n_sim) \
                     .reshape((n_sim, n_samp))
 
-    y_scores = [__randomize_score(s, y_true, acc) for s in init_scores]
+    y_scores = [__randomize_score(s, y_true, acc, tresh) for s in init_scores]
 
     return y_true, np.array(y_scores)
 
@@ -108,6 +127,7 @@ def plot_simulations(n_sim: int,
                      ppos=0.5,
                      acc=0.0,
                      q=0.9,
+                     tresh=0.5,
                      plot_all=False):
     """
     Simulate nullmodels and plot q-th quantile of results.
@@ -127,14 +147,26 @@ def plot_simulations(n_sim: int,
         a treshold of 0.5.
         Must be between 0 and 1.
         Defualt is 0.0.
+    tresh : float
+        Classification treshold when using `acc` to rig the model. Consider
+        a data point positive when `y_score >= tresh`.
+        Must be between 0 and 1.
+        Defualt is 0.5.
+
     plot_all : boolean
         Wether to plot all simulations.
         Defualt is False.
+
+    Examples
+    -------
+    >>> from nullmodel_precision_recall import plot_simulations
+    >>> plot_simulations(100, 1000, ppos=0.5, acc=0.0)
     """
     y_true, y_scores = simulate_nullmodels(n_sim,
                                            n_samp,
                                            ppos=ppos,
-                                           acc=acc)
+                                           acc=acc,
+                                           tresh=tresh)
 
     simulated_curves = [precision_recall_curve(y_true, y_s)[:2]
                         for y_s
